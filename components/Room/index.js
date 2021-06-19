010744ef7d6c02f437a603bc19e066d05b9de5bd
advisorrry <sypikeh@yandex.com>
2021-06-19 16:45:08 +0500

    card and room for roomsPage

---
+++ components/Room/index.js
@@ -0,0 +1,31 @@
+import clsx from 'clsx'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from '../Button'
+
+import styles from './Room.module.scss'
+
+export const Room = ({ title }) => {
+    return (
+        <div className={styles.wrapper}>
+            <div className="d-flex align-items-center justify-content-between">
+                <h2>{title}</h2>
+                <div className={clsx('d-flex align-items-center', styles.actionButtons)}>
+                    <Link href="/rooms">
+                        <Button color="gray" className={styles.leaveButton}>
+                            <img width={18} height={18} src="/static/peace.png" alt="Hand black" />
+                            Leave quietly
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="users">
+                {/* {isLoading && <div className="loader"></div>} */}
+                {/* {users.map((obj) => (
+              <User {...obj} />
+            ))} */}
+            </div>
+        </div>
+    )
+}
