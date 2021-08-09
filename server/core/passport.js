73f7869c03ef4d0d166e388cd7752738ea118822
Alexander <SYPIKEH@yandex.com>
2021-08-09 21:23:00 +0500

    github passport settings

---
+++ server/core/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport') 
+const GitHubStrategy = require('passport-github2').Strategy
+const dotenv = require('dotenv')
+dotenv.config()
+
+passport.use('github',
+    new GitHubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: 'http://localhost:3001/auth/github/callback',
+        },
+        (accessToken, refreshToken, profile, cb) => {
+           console.log(accessToken, refreshToken, profile, cb);
+        },
+    ),
+)
+
+module.exports = passport
